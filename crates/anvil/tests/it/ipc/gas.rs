fe041bf95b375981d0731dacbaf3bc6262920152
Joseph Noorchashm <joseph@sorellalabs.xyz>
2023-08-24 00:21:34 -0700

    fixing multi thread tests

--- crates/anvil/tests/it/ipc/gas.rs
+++ crates/anvil/tests/it/ipc/gas.rs
@@ -45,6 +45,9 @@ async fn test_basefee_half_block() {
     )
     .await;
     let provider = handle.http_provider();
+    let next_base_fee =
+    provider.get_block(BlockNumber::Latest).await.unwrap().unwrap().base_fee_per_gas.unwrap();
+    println!("{:?}", next_base_fee);
     let tx = TransactionRequest::new().to(Address::random()).value(1337u64);
     println!("{:?}", tx);
     provider.send_transaction(tx.clone(), None).await.unwrap().await.unwrap().unwrap();
@@ -53,6 +56,8 @@ async fn test_basefee_half_block() {
     provider.send_transaction(tx.clone(), None).await.unwrap().await.unwrap().unwrap();
     let next_base_fee =
         provider.get_block(BlockNumber::Latest).await.unwrap().unwrap().base_fee_per_gas.unwrap();
+    println!("{:?}", next_base_fee);
+
 
     // unchanged, half block
     assert_eq!(next_base_fee.as_u64(), INITIAL_BASE_FEE);
