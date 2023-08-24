a88e299ba493cfd97c65b80cc35e0e4533f723d9
Joseph Noorchashm <joseph@sorellalabs.xyz>
2023-08-23 23:00:47 -0700

    fixed serial tests

--- crates/anvil/tests/it/ipc/fork.rs
+++ crates/anvil/tests/it/ipc/fork.rs
@@ -631,8 +631,7 @@ async fn test_fork_base_fee() {
     let _res = provider.send_transaction(tx, None).await.unwrap().await.unwrap().unwrap();
 }
 
-#[tokio::test]
-#[serial]
+#[tokio::test(flavor = "multi_thread")]
 async fn test_fork_init_base_fee() {
     let (api, handle) = spawn(fork_config().with_fork_block_number(Some(13184859u64))).await;
 
